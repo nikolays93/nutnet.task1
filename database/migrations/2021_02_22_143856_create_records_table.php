934de536f2f85b244ea94a3340756bf27a1bc241
Nick Sergeevich <nikolays93@ya.ru>
2021-02-23 21:11:36 +0400

    Record model&migrate&controller

---
+++ database/migrations/2021_02_22_143856_create_records_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // Record name.
+            $table->string('name');
+
+            $table->timestamps();
+
+            // Text about this record.
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
