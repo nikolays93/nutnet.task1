389d47482173ddb3cdffdb309a2df1626cde2d54
Nick Sergeevich <nikolays93@ya.ru>
2021-02-23 21:13:26 +0400

    Record views

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,6 +13,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
+
+                    <a href="{{ route('records') }}">Пластинки</a>
                 </div>
             </div>
         </div>
