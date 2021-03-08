ad0e034a6d8a2249af5823c4c62f1ebefe026f14
Nick Sergeevich <nikolays93@ya.ru>
2021-03-08 19:26:21 +0400

    Add IDE tips

--- app/Record.php
+++ app/Record.php
@@ -2,8 +2,13 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Record
+ * @mixin Builder
+ */
 class Record extends Model
 {
 }
